3f57dee5f619e3b35efacb7bf9dd6b3e4418d1ed
Anemaygi <gianemayumisan@gmail.com>
2023-04-02 23:25:16 -0600

    responsividade e conteúdo

--- script.js
+++ script.js
@@ -1,49 +1,64 @@
 function loadContent(url) {
-    if (url === 'projects') {
-      fetch('data/data.json')
-        .then(response => response.json())
-        .then(data => {
-          const div = document.getElementById("content");
-          div.innerHTML = "<h1>Projetos</h1>";
-          data.projects.forEach(item => {
-            const newDiv = document.createElement("div");
-            newDiv.classList.add('items');
-            newDiv.innerHTML = `
-            
-            <h2>${item.name}</h2>
-            <p>${item.description}</p>
-            
-            `;
-            div.appendChild(newDiv);
-          });      
-        })
-        .catch(error => console.log(error));
-    } else if(url === 'experience'){
-        fetch('data/data.json')
-        .then(response => response.json())
-        .then(data => {
-          const div = document.getElementById("content");
-          div.innerHTML = "";
-          data.academicexperience.forEach(item => {
-            const newDiv = document.createElement("div");
-            newDiv.innerHTML = `
-            
-            <h2>${item.name}</h2>
-            <p>${item.description}</p>
-            
-            `;
-            div.appendChild(newDiv);
-          });      
-        })
-        .catch(error => console.log(error));
-    }
-    else {
-      fetch(url)
-       .then(response => response.text())
-       .then(data => {
-         document.getElementById("content").innerHTML = data;
-       });
-    }
+  if (url === 'projects') {
+    fetch('data/data.json')
+      .then(response => response.json())
+      .then(data => {
+        const div = document.getElementById("contentstuff");
+        div.innerHTML = "<h1>Projects (em construção)</h1>";
+        const wrapper = document.createElement("div");
+        wrapper.classList.add('wrapperitems')
+        data.projects.forEach(item => {
+          const newDiv = document.createElement("div");
+          newDiv.classList.add('items');
+          newDiv.innerHTML = `
+          
+          <h2>${item.name}</h2>
+          <p>${item.description}</p>
+          
+          `;
+          wrapper.appendChild(newDiv);
+        });      
+      div.appendChild(wrapper);
+      })
+      .catch(error => console.log(error));
+  } 
+  else if (url === 'resume'){
+    const urlcv = "https://drive.google.com/uc?export=download&id=1VcFq2NLkNFlFwZXJLxO6uynK5czSQU6z";
+    const pdfcv = "src/ProfessionalCV_En.pdf"
+    const div = document.getElementById("contentstuff");
+    div.innerHTML = `
+      <center><a class="buttond" href="${urlcv}" target="_blank"> Download CV </a></center>
+<iframe src="${pdfcv}" width="100%" height="500px"></iframe>
+    `
   }
+    
+  // else if(url === 'experience'){
+  //     fetch('data/data.json')
+  //     .then(response => response.json())
+  //     .then(data => {
+  //       const div = document.getElementById("contentstuff");
+  //       div.innerHTML = "<h1>Experience</h1>";
+  //       data.academicexperience.forEach(item => {
+  //         const newDiv = document.createElement("div");
+  //         newDiv.classList.add('items');
+  //         newDiv.innerHTML = `
+          
+  //         <h2>${item.name}</h2>
+  //         <p>${item.description}</p>
+          
+  //         `;
+  //         div.appendChild(newDiv);
+  //       });       
+  //     })
+  //     .catch(error => console.log(error));
+  // }
   
-  
+  else {
+    fetch(url)
+     .then(response => response.text())
+     .then(data => {
+       document.getElementById("contentstuff").innerHTML = data;
+     });
+  }
+}
+
